b7e90b85703b8de7395c20f732db144ef1326e24
Ma Tianqi <mtq1997@126.com>
2021-04-03 00:37:35 +0800

    test: server call

--- packages/server/__tests__/call.spec.ts
+++ packages/server/__tests__/call.spec.ts
@@ -1,39 +1,38 @@
-// import { NumberType, StringType, createJSONCall } from '../../builder/src/index'
+import { NumberType, StringType, createJSONCallType } from 'jc-builder'
+import { createJSONCall } from '../src/index'
 
-// describe('call', () => {
-//   it('simple', () => {
-//     const call = createJSONCall(
-//       'call',
-//       NumberType,
-//       StringType
-//     )((input) => {
-//       return '' + input
-//     })
+describe('call', () => {
+  it('simple', () => {
+    const call = createJSONCall(
+      createJSONCallType('call', NumberType, StringType)
+    )((input) => {
+      return '' + input
+    })
 
-//     expect(call('foo')).toMatchObject({
-//       expected: 'number',
-//       accept: '"foo"',
-//     })
-//     expect(call(0)).toBe('0')
-//     expect(call(true)).toMatchObject({
-//       expected: 'number',
-//       accept: 'true',
-//     })
-//     expect(call(null)).toMatchObject({
-//       expected: 'number',
-//       accept: 'null',
-//     })
-//     expect(call({})).toMatchObject({
-//       expected: 'number',
-//       accept: '{}',
-//     })
-//     expect(call([])).toMatchObject({
-//       expected: 'number',
-//       accept: '[]',
-//     })
-//     expect(call(undefined)).toMatchObject({
-//       expected: 'number',
-//       accept: undefined,
-//     })
-//   })
-// })
+    expect(call('foo')).toMatchObject({
+      expected: 'number',
+      accept: '"foo"',
+    })
+    expect(call(0)).toBe('0')
+    expect(call(true)).toMatchObject({
+      expected: 'number',
+      accept: 'true',
+    })
+    expect(call(null)).toMatchObject({
+      expected: 'number',
+      accept: 'null',
+    })
+    expect(call({})).toMatchObject({
+      expected: 'number',
+      accept: '{}',
+    })
+    expect(call([])).toMatchObject({
+      expected: 'number',
+      accept: '[]',
+    })
+    expect(call(undefined)).toMatchObject({
+      expected: 'number',
+      accept: undefined,
+    })
+  })
+})
