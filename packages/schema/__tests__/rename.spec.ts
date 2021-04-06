8d51b8acab3ce648fd5049b327a557eac6e9b879
tqma <tqma@trip.com>
2021-04-06 22:16:38 +0800

    schema: check some

--- packages/schema/__tests__/rename.spec.ts
+++ packages/schema/__tests__/rename.spec.ts
@@ -1,3 +1,45 @@
+import path from 'path'
+import {
+  rename,
+  createSchema,
+  createSchemaModule,
+  createLinkDefination,
+  Namer,
+  check,
+} from '../src'
+
 describe('rename', () => {
-  it.todo('work')
+  it('sample', () => {
+    const foo = path.resolve(__dirname, './foo.jc')
+    const bar = path.resolve(__dirname, './bar.jc')
+    const baz = path.resolve(__dirname, './baz.jc')
+
+    const prev = createSchema(foo)
+    const fooModule = createSchemaModule(foo)
+    const barModule = createSchemaModule(bar)
+    barModule.linkDefinations.push(
+      createLinkDefination('./foo.jc', [['foo6', 'foo']])
+    )
+    const bazModule = createSchemaModule(baz)
+    bazModule.linkDefinations.push(
+      createLinkDefination('./foo.jc', [['foo6', 'foo']])
+    )
+    bazModule.linkDefinations.push(
+      createLinkDefination('./bar.jc', [
+        ['bar', 'bar'],
+        ['fooAndBar', 'fooAndBar'],
+      ])
+    )
+
+    prev.modules.push(fooModule)
+    prev.modules.push(barModule)
+    prev.modules.push(bazModule)
+
+    const getName: Namer = (id, isExist) => {
+      return path.basename(id)
+    }
+    const schema = rename(prev, getName)
+
+    expect(check(schema)).toBe(null)
+  })
 })
--- packages/schema/src/check.ts
+++ packages/schema/src/check.ts
@@ -1,6 +1,88 @@
 import { SchemaError } from './error'
-import type { Schema } from './schema'
+import type {
+  Schema,
+  SchemaModule,
+  TypeDefination,
+  DeriveDefination,
+  CallDefination,
+  LinkDefination,
+  ExportDefination,
+} from './schema'
 
 export const check = (schema: Schema): SchemaError | null => {
+  const checkSchemaModule = (module: SchemaModule): SchemaError | null => {
+    const checkTypeDefination = (
+      typeDefination: TypeDefination
+    ): SchemaError | null => {
+      return null
+    }
+
+    const checkDeriveDefination = (
+      deriveDefination: DeriveDefination
+    ): SchemaError | null => {
+      return null
+    }
+
+    const checkCallDefination = (
+      callDefination: CallDefination
+    ): SchemaError | null => {
+      return null
+    }
+
+    const checkLinkDefination = (
+      linkDefination: LinkDefination
+    ): SchemaError | null => {
+      return null
+    }
+
+    const checkExportDefination = (
+      exportDefination: ExportDefination
+    ): SchemaError | null => {
+      return null
+    }
+
+    for (const linkDefination of module.linkDefinations) {
+      const result = checkLinkDefination(linkDefination)
+
+      if (result !== null) return result
+    }
+
+    for (const typeDefination of module.typeDefinations) {
+      const result = checkTypeDefination(typeDefination)
+
+      if (result !== null) return result
+    }
+
+    for (const deriveDefination of module.deriveDefinations) {
+      const result = checkDeriveDefination(deriveDefination)
+
+      if (result !== null) return result
+    }
+
+    for (const callDefination of module.callDefinations) {
+      const result = checkCallDefination(callDefination)
+
+      if (result !== null) return result
+    }
+
+    for (const exportDefination of module.exportDefinations) {
+      const result = checkExportDefination(exportDefination)
+
+      if (result !== null) return result
+    }
+
+    return null
+  }
+
+  if (!schema.modules.some((module) => module.id === schema.entry)) {
+    return new SchemaError(`Unknown schema entry`, schema.entry)
+  }
+
+  for (const module of schema.modules) {
+    const result = checkSchemaModule(module)
+
+    if (result !== null) return result
+  }
+
   return null
 }
